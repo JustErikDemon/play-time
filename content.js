402c6b015f25f1f77ab4ec0413bb9f82ce644f01
JustErikDemon <160011349+JustErikDemon@users.noreply.github.com>
2024-10-26 15:05:05 +0200

    Update content.js

--- content.js
+++ content.js
@@ -8,17 +8,16 @@ function logPresenceData() {
     }
 }
 
-// Check if the current URL matches the target game URL
+// Check if the current URL matches the Roblox home URL
 const targetUrl = 'https://www.roblox.com/home';
 
 if (window.location.href === targetUrl) {
-    console.log(`You are on the target game page: ${targetUrl}`);
+    console.log(`You are on the Roblox home page: ${targetUrl}`);
     // Log PresenceData every second while on the page
     setInterval(logPresenceData, 1000); // Log PresenceData every second
 } else {
-    console.log(`You are not on the target game page. Current URL: ${window.location.href}`);
+    console.log(`You are not on the Roblox home page. Current URL: ${window.location.href}`);
 }
-
 
 
 
